0ed893f11fd82e621ca2637aa52d2afc5fe56dd8
Your Name <you@example.com>
2023-04-15 18:25:30 +0500

    add all files

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+// import "app"
+// // import 	"github.com/gorilla/mux"
+// // import "net/http"
+// // import 	"fmt"
+
+// // func myHandler(w http.ResponseWriter, r *http.Request) {
+// // 	fmt.Fprintf(w, "Hello, world!")
+// //   }
+// func main() {
+// 	app := &app.App{}
+// 	app.Initialize()
+// 	app.Run(":3000")
+
+// }
